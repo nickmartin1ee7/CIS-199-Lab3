68436ff6ec3c5467e8b053fa87c61ed29965c8b7
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-11-26 15:49:07 -0500

    Committing old repo

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calcButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radiusOfSphereTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.diameterResultLabel = new System.Windows.Forms.Label();
+            this.surfaceAreaResultLabel = new System.Windows.Forms.Label();
+            this.volumeResultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(285, 102);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 1;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radiusOfSphereTextBox
+            // 
+            this.radiusOfSphereTextBox.Location = new System.Drawing.Point(272, 76);
+            this.radiusOfSphereTextBox.Name = "radiusOfSphereTextBox";
+            this.radiusOfSphereTextBox.Size = new System.Drawing.Size(100, 20);
+            this.radiusOfSphereTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Radius of sphere: ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(222, 199);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Diameter: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Surface Area: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Volume: ";
+            // 
+            // diameterResultLabel
+            // 
+            this.diameterResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.diameterResultLabel.Location = new System.Drawing.Point(93, 220);
+            this.diameterResultLabel.Name = "diameterResultLabel";
+            this.diameterResultLabel.Size = new System.Drawing.Size(100, 20);
+            this.diameterResultLabel.TabIndex = 11;
+            // 
+            // surfaceAreaResultLabel
+            // 
+            this.surfaceAreaResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.surfaceAreaResultLabel.Location = new System.Drawing.Point(93, 263);
+            this.surfaceAreaResultLabel.Name = "surfaceAreaResultLabel";
+            this.surfaceAreaResultLabel.Size = new System.Drawing.Size(100, 20);
+            this.surfaceAreaResultLabel.TabIndex = 12;
+            // 
+            // volumeResultLabel
+            // 
+            this.volumeResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.volumeResultLabel.Location = new System.Drawing.Point(93, 306);
+            this.volumeResultLabel.Name = "volumeResultLabel";
+            this.volumeResultLabel.Size = new System.Drawing.Size(100, 20);
+            this.volumeResultLabel.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.volumeResultLabel);
+            this.Controls.Add(this.surfaceAreaResultLabel);
+            this.Controls.Add(this.diameterResultLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radiusOfSphereTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.calcButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lab 3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox radiusOfSphereTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label diameterResultLabel;
+        private System.Windows.Forms.Label surfaceAreaResultLabel;
+        private System.Windows.Forms.Label volumeResultLabel;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,44 @@
+﻿/**
+ *  Grading ID:     L8486
+ *  Course:         199-72
+ *  Lab:            3
+ *  Due:            9/22/19
+ *  Description:    Sphere calulator program that will accept a radius as input, then calculate and output diameter, surface area, and volume of a sphere.
+ **/
+
+using System;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When calculate button is clicked, calculate diameter, surface area, and volume values, then update result labels
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // Variables
+            double radius,      // Radius value of sphere
+                   diameter,    // Calculated diameter value of sphere
+                   surfaceArea, // Calculated surface area value of sphere
+                   volume;      // Calculated volume value of sphere
+
+            // Input
+            radius = double.Parse(radiusOfSphereTextBox.Text);  // Parse input of radius textbox into radius value
+
+            // Calculations
+            diameter = radius * 2;                              // Mathmatical formula to calculate diameter using a radius
+            surfaceArea = 4 * Math.PI * Math.Pow(radius, 2);    // Mathmatical formula to calculate surface area using radius
+            volume = (4 * Math.PI * Math.Pow(radius, 3)) / 3;   // Mathmatical formula to calculate volume using radius
+
+            // Output
+            diameterResultLabel.Text = $"{diameter:f2}";         // Update diameter result label's text
+            surfaceAreaResultLabel.Text = $"{surfaceArea:f2}";   // Update surface result label's text
+            volumeResultLabel.Text = $"{volume:f2}";             // Update volume result label's text
+        }
+    }
+}
